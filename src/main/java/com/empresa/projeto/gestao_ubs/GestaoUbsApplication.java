375f6ebc56520c6a8cd33e400f884ca30574f639
Guilherme <albuquerque_me301@outlook.com>
2025-12-27 15:44:27 -0300

    Initial commit of Spring Boot Project

---
+++ src/main/java/com/empresa/projeto/gestao_ubs/GestaoUbsApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.projeto.gestao_ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoUbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoUbsApplication.class, args);
+	}
+
+}
